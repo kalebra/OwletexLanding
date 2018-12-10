69ad84a6422ad0c9cb53193c2f96b0bd21d89baa
unknown <miket@meta.ua>
2018-12-10 19:16:06 +0200

    end of day

--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ $(function() {
             $(this).addClass("active");
         }
     });
+
+    $(".quick-answers .question-head").click(function () {
+        if (!$(this).hasClass('active')) {
+            $(".quick-answers .question-head").removeClass('active');
+            $(".quick-answers .question-body").slideUp();
+            $(this).addClass('active');
+            $(this).next().slideDown();
+        } else {
+            $(".quick-answers .question-head").removeClass('active');
+            $(".quick-answers .question-body").slideUp();
+        }
+    })
+
+    $('.news-cards').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 });
